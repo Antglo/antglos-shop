d4956336e9b39ff94972ccf7df4ee40247e1484f
Antglo <jtab1277@gmail.com>
2023-12-14 15:08:58 -0500

    User Creation Logic Added

--- modules/auth.py
+++ modules/auth.py
@@ -1,4 +1,5 @@
 from flask import Flask, Blueprint, render_template, redirect, url_for, send_from_directory, request, flash
+from werkzeug.security import generate_password_hash
 from flask import Blueprint
 from .models import User
 from app import db
@@ -16,28 +17,43 @@ def login_page():
 
 		if user and user.check_password(password):
 			flash('Login Successful!', 'success')
-			return redirect(url_for('landing_page'))
+			return redirect(url_for('main.landing_page'))
 	return render_template('/auth/login.html')
 
 #registration
+# @auth.route('/register')
+# def register_load():
+#     return render_template('register.html')
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
+	'''function for registration logic; registration success/fail'''
 	if request.method == 'POST':
 		username = request.form.get('username')
 		password = request.form.get('password')
 
+		#Check if poth fields are contain text
 		if not username or not password:
 			flash('Both Username and password are required.', 'error')
-		elif User.query.filter_by(username=username).first():
+			#Let user try again
+			return redirect(url_for('auth.register'))
+
+		#check table.user if username is taken
+		user =	User.query.filter_by(username=username).first()
+		if user:
 			flash('The USERNAME is already taken.', 'error')
-		else:
-			new_user = User(username=username)
-			new_user.set_password(password)
-			db.session.add(new_user)
-			db.session.commit()
-			flash('Registered Account Successfully!', 'success')
-			return redirect(url_for('login_page'))
-	return render_template('/auth/register.html')
+			#Redirect (reload page) to allow user to try again
+			return redirect(url_for('auth.register'))
+
+
+		#If user form is correct; create user row in 'schema.db'
+		new_user = User(username=username, password=generate_password_hash(password, method='pbkdf2:sha256'))
+		#new_user.set_password(password)
+		db.session.add(new_user)
+		db.session.commit()
+		flash('Registered Account Successfully!', 'success')
+		return redirect(url_for('auth.login_page'))
+	else:
+		return render_template('/auth/register.html')
 
 @auth.route('/logout')
 def logout():
